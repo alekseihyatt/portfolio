5104b65ee73d4aded9dcbad752739b70da10b781
Aleksey Arsentev <useruser.edd@icloud.com>
2024-03-13 17:07:32 +0300

    first commit

--- main.js
+++ main.js
@@ -28,4 +28,18 @@ function scrollIntoMiddle(elementId) {
     });
 }
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 
