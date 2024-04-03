904f624588a0d1655f2ffb95ee5ce6ef6e91bb4e
Aleksey Arsentev <useruser.edd@icloud.com>
2024-04-03 18:50:35 +0300

    changed overflow and background

--- main.js
+++ main.js
@@ -29,3 +29,14 @@ function scrollIntoMiddle(elementId) {
         behavior: 'smooth'
     });
 }
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
